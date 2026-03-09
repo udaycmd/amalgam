5a238bb204ce5abd385b9eaa0e81361248de5362
Uday Tiwari <udaytiwari720@gmail.com>
2026-03-09 21:58:34 +0530

    Add db logic for /threads route

--- priory/routes/thread.ts
+++ priory/routes/thread.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import db from "@/prisma/db.js";
 
 const threadRouter = Router({ mergeParams: true });
 
@@ -6,13 +7,31 @@ threadRouter.get("/", async (req, res) => {
   const { channel } = req.params as { channel: string };
 });
 
-threadRouter.get("/:threadId", (req, res) => {
+threadRouter.get("/:threadId", async (req, res) => {
   const { threadId, channel } = req.params as {
     threadId: string;
     channel: string;
   };
 
-  res.status(200).send(`routed to thread ${channel}/${threadId}`);
+  const thread = await db.thread.findFirst({
+    where: {
+      id: BigInt(threadId),
+      channel: {
+        slug: channel,
+      },
+    },
+    include: {
+      posts: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  if (!thread) {
+    return res.status(404).json({ error: "Thread not found" });
+  }
 });
 
 export default threadRouter;
